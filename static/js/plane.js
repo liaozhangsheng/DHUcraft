f32cd54214c85c2428db838df3e7951449d4108c
liaozhangsheng <liaozhangsheng@163.com>
2023-01-02 21:06:00 +0800

    update index.html

--- static/js/plane.js
+++ static/js/plane.js
@@ -1,22 +1,36 @@
-let plane = document.getElementById('plane');
-let deg = 0, ex = 0, ey = 0, vx = 0, vy = 0, count = 0;
-window.addEventListener('mousemove', (e) => {
-    ex = e.x - plane.offsetLeft - plane.clientWidth / 2;
-    ey = e.y - plane.offsetTop - plane.clientHeight / 2;
-    deg = 360 * Math.atan(ey / ex) / (2 * Math.PI) + 45;
+let plane = document.getElementById("plane");
+let deg = 0,
+    ex = 0,
+    ey = 0,
+    vx = 0,
+    vy = 0,
+    count = 0;
+
+window.addEventListener("mousemove", (e) => {
+    ex = e.x - plane.offsetLeft - plane.clientWidth / 2 + window.scrollX;
+    ey = e.y - plane.offsetTop - plane.clientHeight / 2 + window.scrollY;
+    deg = (360 * Math.atan(ey / ex)) / (2 * Math.PI) + 45;
     if (ex < 0) {
         deg += 180;
     }
     count = 0;
-})
+});
+
+window.addEventListener("scroll", (e) => {
+    plane.style.left = vx + window.scrollX + "px";
+    plane.style.top = vy + window.scrollY + "px";
+});
+
 function draw() {
-    plane.style.transform = 'rotate(' + deg + 'deg)';
+    requestAnimationFrame(draw);
+    plane.style.transform = "rotate(" + deg + "deg)";
     if (count < 100) {
-        vx += ex / 100;
-        vy += ey / 100;
+        vx += ex / 70;
+        vy += ey / 70;
     }
-    plane.style.left = vx + 'px';
-    plane.style.top = vy + 'px';
+    plane.style.left = vx + window.scrollX + "px";
+    plane.style.top = vy + window.scrollY + "px";
     count++;
 }
-setInterval(draw, 1);
+
+draw();
